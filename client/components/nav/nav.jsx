fac6f356d65fe64ce0e0e098c6b2f7f9599c0655
ibaena <ivan.baena04@gmail.com>
2016-11-24 20:08:53 -0500

    -solved nav bg firing issue; 3/4 created img asssets

--- client/components/nav/nav.jsx
+++ client/components/nav/nav.jsx
@@ -3,10 +3,10 @@ import { mount } from 'react-mounter';
 
 Nav = React.createClass({
   menu:[
-    {_id:'0',name:'Home', background:'images/nav/fevermac.png', animate:'', color:'' },
-    {_id:'1',name:'Amloid Toys', background:'images/nav/amloidipad.png', animate:'', color:'' },
-    {_id:'2',name:'Feverall', background:'', animate:'', color:'' },
-    {_id:'3',name:'Nice n Clean', background:'', animate:'', color:'' },
+    {_id:'0',name:'Home', background:'', animate:'', color:'' },
+    {_id:'1',name:'Amloid Toys', background:'images/nav/bothamloid.png', animate:'', color:'' },
+    {_id:'2',name:'Feverall', background:'images/nav/feverthree.png', animate:'', color:'' },
+    {_id:'3',name:'Nice n Clean', background:'images/nav/nicencleanboth.png', animate:'', color:'' },
     {_id:'4',name:'Wine 4 Food', background:'', animate:'', color:'' },
   ],
   componentDidMount(){
@@ -32,9 +32,9 @@ Nav = React.createClass({
 
         <li className="navlist-item" key={item._id}>
           <a href="#" className="white-text nav-link alwaysTop">{item.name}
-            <img src={item.background} className="nav-bg hide-nav-bg animated" alt="feverall" />
+            <img src={item.background} className="nav-bg hide-nav-bg animated center-img" alt={item.name} />
           </a>
-          </li>
+        </li>
       )
     })
     return (
@@ -45,7 +45,7 @@ Nav = React.createClass({
           <div className="bar bar-bot"></div>
         </btn>
         <div id="nav-wrapper" className="container-fluid no-padding animated slideOutUp">
-          <ul className="navlist vcenter">
+          <ul className="navlist vcenter col-xs-12">
             {links}
           </ul>
         </div>
