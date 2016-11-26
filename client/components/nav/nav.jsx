9325af66e700fb9f33f2bc5eed12c1ec613cb7aa
ibaena <ivan.baena04@gmail.com>
2016-11-26 01:46:38 -0500

    75% nav

--- client/components/nav/nav.jsx
+++ client/components/nav/nav.jsx
@@ -9,11 +9,22 @@ Nav = React.createClass({
     {_id:'3',name:'Nice n Clean', background:'images/nav/nicencleanboth.png', class:'nav-bg hide-nav-bg animated center-img', color:'' },
     {_id:'4',name:'Wine 4 Food', background:'images/nav/wineboth.png', class:'nav-bg hide-nav-bg animated center-img', color:'' },
   ],
+  contact:[
+    {_id:'0', name:'TWITTER', link:'https://twitter.com/NotThefakeIB'},
+    {_id:'1', name:'LINKEDIN', link:'https://www.linkedin.com/in/ivanbaena'},
+    {_id:'2', name:'GITHUB', link:'https://github.com/ibaena'},
+    {_id:'3', name:'IVANBAENA04 [AT] GMAIL.COM', link:'ivan.baena04@gmail.com'},
+  ],
   componentDidMount(){
     $('#nav-wrapper').hide();
     $('#nav-btn').on('click',function() {
       $('#nav-wrapper').show();
       $('#nav-wrapper').toggleClass('slideOutUp slideInDown')
+        setTimeout(function(){
+          $('.navlist').toggleClass('hide-nav-bg fadeIn');
+          $('.contactlist').toggleClass('hide-nav-bg fadeIn');
+        }, 0600);
+
     })
     $(".btn-nav").on("click tap", function(){
       $(".bar").toggleClass("animatedButton");
@@ -42,7 +53,16 @@ Nav = React.createClass({
           </a>
         </li>
       )
-    })
+    });
+    var contact = this.contact.map(function(item){
+      return (
+
+        <li className="contactlist-item" key={item._id}>
+          <a href={item.link} target="_blank" className="white-text contact-link">{item.name}
+          </a>
+        </li>
+      )
+    });
     return (
       <div>
         <btn className="btn-nav btn-square-fixed" id="nav-btn">
@@ -51,8 +71,11 @@ Nav = React.createClass({
           <div className="bar bar-bot"></div>
         </btn>
         <div id="nav-wrapper" className="container-fluid no-padding animated slideOutUp">
-          <ul className="navlist vcenter col-xs-12">
+          <ul className="navlist vcenter col-xs-12 hide-nav-bg">
             {links}
+          </ul>
+          <ul className="contactlist vcenter col-xs-12 hide-nav-bg">
+            {contact}
           </ul>
         </div>
       </div>
