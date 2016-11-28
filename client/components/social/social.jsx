a49f154aef8289496cd4ee726027ca0808ea757e
Ivan Baena <ivan.baena04@gmail.com>
2016-11-28 14:08:40 -0500

    removed extra css code in projects ahd to redo media queries as a result

--- client/components/social/social.jsx
+++ client/components/social/social.jsx
@@ -4,9 +4,10 @@ import { mount } from 'react-mounter';
 Social = React.createClass({
   render(){
     return (
-      <div className="social-bg magictime hide-project ">
+      <div className="social-bg hide-project ">
         <div id="social-overlay"></div>
-        <div className="container-fluid landing-zdex" >
+        <div className="container-fluid no-padding landing-zdex" >
+        <div className="social-box"></div>
         </div>
       </div>
     )
